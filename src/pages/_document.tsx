a4a10d3857397608090adc0b26a9d1fd711c6e4e
Pedro Duarte <pedrohlduarte@gmail.com>
2021-12-12 10:03:59 -0300

    0.8.0

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="Site da empresa NeXTIME, temos como visão: otimizar o tempo através do desenvolvimento de novas tecnologias, produtos inovadores, soluções criativas e educação. Possibilitando aos indivíduos gastarem suas energias realizando os seus sonhos." />
+          <meta name="description" content="Odonto SOS tem atendimentos 24 horas de urgência odontológica em BH e região metropolitana de Belo Horizonte. Oferece dentistas de plantão 24 horas na região da Savassi em BH." />
           <meta name="keywords" content="urgência odontológica, dentista 24 horas, dentista 24 horas BH, dentista de plantão 24 horas, plantão odontológico, odontologia 24 horas, clínica dentária 24 horas, dentista atendimento 24 horas, dentista emergência" />
           <link rel="icon" href="/favicon.png" />
           {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-YBBVTWFKF4" />
